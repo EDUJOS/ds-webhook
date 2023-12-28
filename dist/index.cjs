347cf758dbf94f8806c89e4d51089d15b283b18a
Ed <josuegomo@hotmail.com>
2023-12-28 18:37:10 -0500

    🔖 v1.0.14

--- dist/index.cjs
+++ dist/index.cjs
@@ -95535,35 +95535,37 @@ async function sendDiscordNotify (
     core.warning('Recuerdda proporcionar los parámetros requeridos, si no sabes cuales son visita https://github.com/edujos/ds-webhook/#discord-webhook-github-action')
     core.ExitCode(1)
   } else {
-    let newDescription = ''
-    description.length > 4000
-      ? newDescription = `${description.substring(0, 4000)}...`
-      : core.warning(`¡Vaya parece que has excedido el límite de caracteres permitidos por discord!\nPor ello hemos omitido el envío de tu webhook\n${description.length}`)
+    if (description.length > 4000) {
+      let newDescription = ''
+      newDescription = `${description.substring(0, 4000)}...`
 
-    const embedToSend = new EmbedBuilder()
-      .setTitle(title)
-      .setDescription(newDescription)
-      .setColor(color)
-      .setTimestamp()
+      const embedToSend = new EmbedBuilder()
+        .setTitle(title)
+        .setDescription(newDescription)
+        .setColor(color)
+        .setTimestamp()
 
-    if (author) embedToSend.setAuthor({ name: author, iconURL: authorIcon, url: authorUrl })
-    if (url) embedToSend.setURL(url)
-    if (color !== '#fff') embedToSend.setColor(color)
-    if (thumbnail) embedToSend.setThumbnail(thumbnail)
-    if (image) embedToSend.setImage(image)
-    if (footer) embedToSend.setFooter({ text: footer, iconURL: footerIcon })
+      if (author) embedToSend.setAuthor({ name: author, iconURL: authorIcon, url: authorUrl })
+      if (url) embedToSend.setURL(url)
+      if (color !== '#fff') embedToSend.setColor(color)
+      if (thumbnail) embedToSend.setThumbnail(thumbnail)
+      if (image) embedToSend.setImage(image)
+      if (footer) embedToSend.setFooter({ text: footer, iconURL: footerIcon })
 
-    try {
-      const webhookClient = new WebhookClient({ url: webhookUrl })
+      try {
+        const webhookClient = new WebhookClient({ url: webhookUrl })
 
-      webhookClient.send({
-        embeds: [embedToSend]
-      })
-    } catch (e) {
-      core.error(`Ups! Ha ocurrido un error inesperado:\nError: ${e.message}`)
-      core.ExitCode(1)
+        webhookClient.send({
+          embeds: [embedToSend]
+        })
+      } catch (e) {
+        core.error(`Ups! Ha ocurrido un error inesperado:\nError: ${e.message}`)
+        core.ExitCode(1)
+      }
+      core.ExitCode(0)
+    } else {
+      core.warning(`¡Vaya parece que has excedido el límite de caracteres permitidos por discord!\nPor ello hemos omitido el envío de tu webhook\n${description.length}`)
     }
-    core.ExitCode(0)
   }
 }
 
