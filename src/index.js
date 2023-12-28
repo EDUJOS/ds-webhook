3f8a166d9ee0d95c47f5ce3d7f7979760cc88225
Ed <josuegomo@hotmail.com>
2023-12-28 16:39:20 -0500

    ✅ Add `discordSendNotify` function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
 import * as core from '@actions/core'
 import github from '@actions/github'
+import { sendDiscordNotify } from './utils/dswebhook'
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet')
-  console.log(`Hello ${nameToGreet}!`)
-  const time = (new Date()).toTimeString()
-  core.setOutput('time', time)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
-} catch (error) {
-  core.setFailed(error.message)
+async function main () {
+  const webhookUrl = core.getInput('webhook_url')
+  const title = core.getInput('title')
+  const description = core.getInput('description')
+  const url = core.getInput('url')
+  const color = core.getInput('color')
+  const footer = core.getInput('footer')
+  const footerIcon = core.getInput('footer_icon')
+  const image = core.getInput('image')
+  const thumbnail = core.getInput('thumbnail')
+  const author = core.getInput('author')
+  const authorUrl = core.getInput('author_url')
+  const authorIcon = core.getInput('author_icon')
+
+  try {
+    await sendDiscordNotify(
+      webhookUrl,
+      author,
+      authorUrl,
+      authorIcon,
+      title,
+      description,
+      url,
+      color,
+      footer,
+      footerIcon,
+      image,
+      thumbnail
+    )
+
+    core.info('El webhook se ha enviado con éxito')
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    core.setFailed(error)
+    process.exit(1)
+  })
---
+++ src/utils/dswebhook.js
@@ -0,0 +1,52 @@
+import { EmbedBuilder, WebhookClient } from 'discord.js'
+import core from '@actions/core'
+
+export async function sendDiscordNotify (
+  webhookUrl,
+  author,
+  authorUrl = 'https://github.com/ERR-Z3R0',
+  authorIcon = 'https://avatars.githubusercontent.com/u/126917905?s=400&u=201c605c76afbdcf6e81d2cd8258d7a1cf42c2da&v=4',
+  title,
+  description,
+  url,
+  color = '#fff',
+  footer,
+  footerIcon = 'https://avatars.githubusercontent.com/u/126917905?s=400&u=201c605c76afbdcf6e81d2cd8258d7a1cf42c2da&v=4',
+  image,
+  thumbnail
+) {
+  if (!webhookUrl | !title | !description) {
+    core.warning('Recuerdda proporcionar los parámetros requeridos, si no sabes cuales son visita https://github.com/edujos/ds-webhook/#discord-webhook-github-action')
+    core.ExitCode(1)
+  } else {
+    let newDescription = ''
+    description.length > 4000
+      ? newDescription = `${description.substring(0, 4000)}...`
+      : core.warning('¡Vaya parece que has excedido el límite de caracteres permitidos por discord!\nPor ello hemos omitido el envío de tu webhook')
+
+    const embedToSend = new EmbedBuilder()
+      .setTitle(title)
+      .setDescription(newDescription)
+      .setColor(color)
+      .setTimestamp()
+
+    if (author) embedToSend.setAuthor({ name: author, iconURL: authorIcon, url: authorUrl })
+    if (url) embedToSend.setURL(url)
+    if (color !== '#fff') embedToSend.setColor(color)
+    if (thumbnail) embedToSend.setThumbnail(thumbnail)
+    if (image) embedToSend.setImage(image)
+    if (footer) embedToSend.setFooter({ text: footer, iconURL: footerIcon })
+
+    try {
+      const webhookClient = new WebhookClient({ url: webhookUrl })
+
+      webhookClient.send({
+        embeds: [embedToSend]
+      })
+    } catch (e) {
+      core.error(`Ups! Ha ocurrido un error inesperado:\nError: ${e.message}`)
+      core.ExitCode(1)
+    }
+    core.ExitCode(0)
+  }
+}
